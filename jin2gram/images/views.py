1621d78e1862bc1339a0b0da031fb03580748e2a
jin-2 <jmakeaz@gmail.com>
2018-06-08 18:34:06 +0900

    Creating list all images view - translate serializer

--- jin2gram/images/views.py
+++ jin2gram/images/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import APIView
+from rest_framework import Response
+from . import models, serializers
 
-# Create your views here.
+class ListAllImage(APIView):
+    
+    def get(self, request, format=None):
+
+        all_image = models.Image.objects.all()
+        serializer = serializers.ImageSerializer(all_image, many=True)
+        return Response(data=serializer.data) 
