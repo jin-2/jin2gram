611082101c04c789dc09961114d6f3daa5ea718d
jin-2 <jmakeaz@gmail.com>
2018-06-13 18:27:55 +0900

    Liking an Image(save DB)

--- jin2gram/images/urls.py
+++ jin2gram/images/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path('', view=views.Feed.as_view(), name="feed"),
-    path('<image_id>/like', view=views.LikeImage.as_view(), name="like"),
+    path('<int:image_id>/like', view=views.LikeImage.as_view(), name="like"),
 ]
