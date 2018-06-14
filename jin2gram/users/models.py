d7fe27b8d6074ffd18b00f7459d61914f3654a65
jin-2 <jmakeaz@gmail.com>
2018-06-14 18:37:15 +0900

    Get people to follower on :explore

--- jin2gram/users/models.py
+++ jin2gram/users/models.py
@@ -16,14 +16,17 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
+    
+    # blank=True 필수값이 아니게 설정한다
+    # null=True DB에 새로운 컬럼을 추가할 때 이미 있었던 데이터들은 빈값으로 채운다
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
-    profile_image = models.ImageField(null=True)
+    profile_image = models.ImageField(blank=True, null=True)
     website = models.URLField(null=True)
     bio = models.TextField(null=True)
     phone = models.CharField(max_length=140, null=True)
     gender = models.CharField(max_length=80, choices=GENDER_CHOICE, null=True)
-    followers = models.ManyToManyField("self")
-    following = models.ManyToManyField("self")
+    followers = models.ManyToManyField("self", blank=True)
+    following = models.ManyToManyField("self", blank=True)
 
     def __str__(self):
         return self.username
