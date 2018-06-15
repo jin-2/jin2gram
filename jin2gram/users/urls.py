ace8db351b6a6618bb9a9b57182f1998a42efb19
jin-2 <jmakeaz@gmail.com>
2018-06-15 19:04:33 +0900

    Getting Following list

--- jin2gram/users/urls.py
+++ jin2gram/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:user_id>/unfollow', view=views.UnFollowUsers.as_view(), name='unfollow_users'),
     path('<username>', view=views.UserProfile.as_view(), name='user_profile'),
     path('<username>/followers', view=views.UserFollowers.as_view(), name='user_followers'),
+    path('<username>/following', view=views.UserFollowing.as_view(), name='user_following'),
 ]
