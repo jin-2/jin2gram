688b4f0f63d550043af8351e8cc15b4ba8c0217e
jin-2 <jmakeaz@gmail.com>
2018-06-18 20:12:55 +0900

    Search users by username

--- jin2gram/users/urls.py
+++ jin2gram/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path('explore', view=views.ExploreUsers.as_view(), name='explore_users'),
+    path('search/', view=views.Search.as_view(), name='search'),
     path('<int:user_id>/follow', view=views.FollowUsers.as_view(), name='follow_users'),
     path('<int:user_id>/unfollow', view=views.UnFollowUsers.as_view(), name='unfollow_users'),
     path('<username>', view=views.UserProfile.as_view(), name='user_profile'),
--- jin2gram/users/views.py
+++ jin2gram/users/views.py
@@ -91,3 +91,20 @@ class UserFollowing(APIView):
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+
+class Search(APIView):
+
+    def get(self, request, format=None):
+
+        username = request.query_params.get('username', None)
+        
+        if username is not None:
+            
+            user = models.User.objects.filter(username__icontains=username)
+            serializer = serializers.ListUserSerializer(user, many=True)
+            
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
