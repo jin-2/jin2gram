ad2d6ff56bcdcb962086ad25b081aa5284e373e8
jin-2 <jmakeaz@gmail.com>
2018-08-26 19:23:56 +0900

    Add Sass to webpack

---
+++ frontend/READ.md
@@ -0,0 +1,36 @@
+# React
+
+## 1. react 설치
+
+## 2. react eject
+
+## 3. scss 사용 설정(dev, prod 둘 다 설정 필요)
+
+```js
+// before
+test: /\.css$/,
+
+// after
+test: /\.(css|scss|sass)$/,
+
+// sass-loader 추가
+{
+loader: require.resolve('sass-loader'),
+    options: {
+        sourcemap: true
+    }
+},
+
+// postcss 소스맵 설정
+sourcemap: ture
+```
+
+### 웹팩의 로더들은 아래에서 위로 로딩을 한다.
+
+1. 'sass-loader'를 이용하여 sass 파일을 css 로 변환한다.
+2. 'postcss-loader'를 이용하여 브라우저 호환성 부여한다.
+3. 'css-loader'는 css 파일을 만든다.
+4. 'style-loader'는 `<head>`안에 스타일 추가한다.
+
+development 환경에서는 빠른 새로고침을 위해 style-loader 를 사용하지만,
+production 환경에서는 다른 로더들이 실패하게 되면 style-loader(fallback 으로)를 사용하게 설정이 되었다.
--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -41,10 +41,11 @@ const cssFilename = 'static/css/[name].[contenthash:8].css';
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
 // However, our output is structured with css, js and media folders.
 // To have this structure working with relative paths, we have to use custom options.
-const extractTextPluginOptions = shouldUseRelativeAssetPaths
-  ? // Making sure that the publicPath goes back to to build folder.
-    { publicPath: Array(cssFilename.split('/').length).join('../') }
-  : {};
+const extractTextPluginOptions = shouldUseRelativeAssetPaths ? // Making sure that the publicPath goes back to to build folder.
+  {
+    publicPath: Array(cssFilename.split('/').length).join('../')
+  } :
+  {};
 
 // This is the production configuration.
 // It compiles slowly and is focused on producing a fast and minimal bundle.
@@ -70,8 +71,8 @@ module.exports = {
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: info =>
       path
-        .relative(paths.appSrc, info.absoluteResourcePath)
-        .replace(/\\/g, '/'),
+      .relative(paths.appSrc, info.absoluteResourcePath)
+      .replace(/\\/g, '/'),
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
@@ -90,7 +91,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -116,16 +117,14 @@ module.exports = {
       {
         test: /\.(js|jsx|mjs)$/,
         enforce: 'pre',
-        use: [
-          {
-            options: {
-              formatter: eslintFormatter,
-              eslintPath: require.resolve('eslint'),
-              
-            },
-            loader: require.resolve('eslint-loader'),
+        use: [{
+          options: {
+            formatter: eslintFormatter,
+            eslintPath: require.resolve('eslint'),
+
           },
-        ],
+          loader: require.resolve('eslint-loader'),
+        }, ],
         include: paths.appSrc,
       },
       {
@@ -149,7 +148,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
@@ -166,18 +165,16 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.(css|scss|sass)$/,
             loader: ExtractTextPlugin.extract(
-              Object.assign(
-                {
+              Object.assign({
                   fallback: {
                     loader: require.resolve('style-loader'),
                     options: {
                       hmr: false,
                     },
                   },
-                  use: [
-                    {
+                  use: [{
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
@@ -203,7 +200,14 @@ module.exports = {
                             flexbox: 'no-2009',
                           }),
                         ],
+                        sourceMap: true
                       },
+                    },
+                    {
+                      loader: require.resolve('sass-loader'),
+                      options: {
+                        sourceMap: true
+                      }
                     },
                   ],
                 },
