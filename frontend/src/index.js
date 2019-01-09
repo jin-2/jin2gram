6f96e6eb50e8db0d26b58a3692c8d2e9bbe513f7
jin-2 <jmakeaz@gmail.com>
2019-01-09 21:38:52 +0900

    #3-11 Syncing React Router with Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+import store, { history } from './redux/configureStore';
 
 store.dispatch({ type: 'shot' });
 
 ReactDOM.render(
     <Provider store={store}>
-        < App />
+        <ConnectedRouter history={history}>
+            < App />
+        </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
 );
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,18 +2,26 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import users from './modules/users';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
-const middleware = [thunk];
-const reducer = combineReducers({
-    users,
-});
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import createHistory from 'history/createBrowserHistory';
 
 const env = process.env.NODE_ENV;
+
+const history = createHistory();
+
+const middleware = [thunk, routerMiddleware(history)];
 
 if (env === 'development') {
     middleware.push(logger);
 }
 
+const reducer = combineReducers({
+    router: connectRouter(history),
+    users,
+});
+
 let configureStore = initialState => createStore(reducer, applyMiddleware(...middleware));
+
+export { history };
 
 export default configureStore();
