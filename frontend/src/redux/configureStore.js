9fa0f5b01516f3c83c8c98565f6033ca01a9209d
jin-2 <jmakeaz@gmail.com>
2019-01-13 19:59:07 +0900

    #3-14 Installing Redux Dev Tools

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -25,7 +25,7 @@ let configureStore;
 
 if (env === 'development') {
     // 개발환경일 때는 Reactotron이랑 store을 생성하여 action을 볼 수 있게 함.
-    configureStore = initialState => Reactotron.createStore(reducer, applyMiddleware(...middleware));
+    configureStore = initialState => Reactotron.createStore(reducer, applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 } else {
     configureStore = initialState => createStore(reducer, applyMiddleware(...middleware));
 }
