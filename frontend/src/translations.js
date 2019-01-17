5cc42e3d205fc0f743e4db68d4d6bd5ca31cf856
jin-2 <jmakeaz@gmail.com>
2019-01-17 19:00:21 +0900

    #3-32 Importing Translations(번역 적용)

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,9 +1,34 @@
 export const translations = {
-    "es": {
-        "Translate this text": "Traduce este texto",
-        "Hello {n}!": "Hola {n}!",
-    },
-    "kr": {
-        "About Us": "Instagram 정보"
-    }
+  'kr': {
+    'Don\'t have an account?': '계정이 없으신가요?',
+    'Sign up': '가입하기',
+    'Have an account?': '계정이 있으신가요?',
+    'Log in': '로그인하기',
+    'Get the App': '앱을 다운로드하세요.',
+    'Download on the App Store': '앱 스토어에서 다운로드 하기',
+    'Get it on Google Play': '구글 플레이에서 다운로드 하기',
+    'Phone number, username, or email': '핸드폰 번호, 사용자 이름 또는 이메일',
+    'Password': '비밀번호',
+    'OR': '또는',
+    'Log in with Facebook': '페이스북으로 로그인하기',
+    'Forgot Password?': '비밀번호를 잊으셨나요?',
+    'Sign up to see photos and videos from your friends.': '친구들의 사진과 비디오를 보려면 가입하세요.',
+    'Mobile Number, or Email': '핸드폰 번호 또는 이메일',
+    'Full Name': '성명',
+    'Username': '사용자 이름',
+    'Sign Up': '회원가입 하기',
+    'About Us': 'Instagram 정보',
+    'Support': '지원',
+    'Press': '홍보 센터',
+    'API': 'API',
+    'Jobs': '채용 정보',
+    'Privacy': '개인정보처리방침',
+    'Terms': '약관',
+    'Directory': '디렉터리',
+    'Profiles': '프로필',
+    'Hashtag': '해시태그',
+    'Language': '언어',
+  },
+  'options': {
+  },
 }
