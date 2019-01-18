cbfef6775719b0fab0382b618a068e362020d6b0
jin-2 <jmakeaz@gmail.com>
2019-01-18 15:33:50 +0900

    #3-35 Login In with Facebook

--- frontend/src/components/LoginForm/container.js
+++ frontend/src/components/LoginForm/container.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import LoginForm from './presenter';
+import React, { Component } from "react";
+import LoginForm from "./presenter";
 
 class Container extends Component {
-    state = {
-        username: "",
-        password: ""
-    };
+  state = {
+    username: "",
+    password: ""
+  };
 
-    render() {
-        const { username, password } = this.state;
-        return <LoginForm handleSubmit={this._handleSubmit} handleInputChange={this._handleInputChange} usernameValue={username} passwordValue={password} />;
-    }
+  render() {
+    const { username, password } = this.state;
+    return (
+      <LoginForm
+        handleSubmit={this._handleSubmit}
+        handleInputChange={this._handleInputChange}
+        handleFbLogin={this._handleFbLogin}
+        usernameValue={username}
+        passwordValue={password}
+      />
+    );
+  }
 
-    _handleInputChange = event => {
-        const { target : { value, name } } = event;
-        this.setState({
-            [name]: value
-        });
-    }
+  _handleInputChange = event => {
+    const {
+      target: { value, name }
+    } = event;
+    this.setState({
+      [name]: value
+    });
+  };
 
-    _handleSubmit = event => {
-        event.preventDefault();
-        console.log(this.state)
-    }
+  _handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  _handleFbLogin = response => {
+    console.log(response);
+  };
 }
 
 export default Container;
--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -1,43 +1,64 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import IconFb from 'react-ionicons/lib/LogoFacebook'
-import formStyles from '../../shared/formStyles.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import FacebookLogin from "react-facebook-login";
+import formStyles from "../../shared/formStyles.scss";
 
 const LoginForm = (props, context) => (
-    <div className={formStyles.loginWrap}>
-        <form onSubmit={props.handleSubmit}>
-            <div className={formStyles.inputField}>
-                <input type="text" name="" id="loginId" placeholder={context.t("Phone number, username, or email")} value={props.usernameValue} name="username" onChange={props.handleInputChange} />
-            </div>
-            <div className={formStyles.inputField}>
-                <input type="password" name="" id="loginPw" placeholder={context.t("Password")} name="password" value={props.passwordValue} onChange={props.handleInputChange} />
-            </div>
-            <div className={formStyles.buttonWrap}>
-                <button className={formStyles.button}>{context.t("Log in")}</button>
-            </div>
-        </form>
-        <div>
-            <p className={formStyles.otherOption}>
-                <span className={formStyles.or}>{context.t("OR")}</span>
-            </p>
-            <button type="button" className={formStyles.buttonFb}>
-                <IconFb fontSize="20px" color="#385185" className={formStyles.icon} />
-                {context.t("Log in with Facebook")}
-            </button>
-            <p className={formStyles.forgot}>{context.t("Forgot Password?")}</p>
-        </div>
+  <div className={formStyles.loginWrap}>
+    <form onSubmit={props.handleSubmit}>
+      <div className={formStyles.inputField}>
+        <input
+          type="text"
+          name=""
+          id="loginId"
+          placeholder={context.t("Phone number, username, or email")}
+          value={props.usernameValue}
+          name="username"
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <div className={formStyles.inputField}>
+        <input
+          type="password"
+          name=""
+          id="loginPw"
+          placeholder={context.t("Password")}
+          name="password"
+          value={props.passwordValue}
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <div className={formStyles.buttonWrap}>
+        <button className={formStyles.button}>{context.t("Log in")}</button>
+      </div>
+    </form>
+    <div>
+      <p className={formStyles.otherOption}>
+        <span className={formStyles.or}>{context.t("OR")}</span>
+      </p>
+      <FacebookLogin
+        appId="2228520780713829"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={props.handleFbLogin}
+        cssClass={formStyles.buttonFb}
+        icon="fa-facebook-square"
+      />
+      <p className={formStyles.forgot}>{context.t("Forgot Password?")}</p>
     </div>
+  </div>
 );
 
 LoginForm.propTypes = {
-    usernameValue: PropTypes.string.isRequired,
-    passwordValue: PropTypes.string.isRequired,
-    handleInputChange: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-}
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleFbLogin: PropTypes.func.isRequired
+};
 
 LoginForm.contextTypes = {
-    t: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired
 };
 
 export default LoginForm;
