8c1ddf9bc7156f16a3f80ac984de5a57cbfa78ad
jin-2 <jmakeaz@gmail.com>
2019-01-18 15:37:57 +0900

    Sign Up with Facebook Coding Challenge

--- frontend/src/components/SignupForm/container.js
+++ frontend/src/components/SignupForm/container.js
@@ -1,40 +1,47 @@
-import React, { Component } from 'react';
-import SignupForm from './presenter';
+import React, { Component } from "react";
+import SignupForm from "./presenter";
 
 class Container extends Component {
-    state = {
-        email: "",
-        fullname: "",
-        username: "",
-        password: "",
-    }
+  state = {
+    email: "",
+    fullname: "",
+    username: "",
+    password: ""
+  };
 
-    render() {
-        const { email, fullname, username, password } = this.state;
-        
-        return (
-            <SignupForm 
-                emailValue={email} 
-                fullnameValue={fullname} 
-                usernameValue={username} 
-                passwordValue={password} 
-                handleInputChange={this._handleInputChange} 
-                handleSubmit={this._handleSubmit}
-            />
-        );
-    }
+  render() {
+    const { email, fullname, username, password } = this.state;
 
-    _handleInputChange = event => {
-        const { target: {value, name} } = event;
-        this.setState({
-            [name]: value
-        });
-    }
+    return (
+      <SignupForm
+        emailValue={email}
+        fullnameValue={fullname}
+        usernameValue={username}
+        passwordValue={password}
+        handleInputChange={this._handleInputChange}
+        handleSubmit={this._handleSubmit}
+        handleFbLogin={this._handleFbLogin}
+      />
+    );
+  }
 
-    _handleSubmit = event => {
-        event.preventDefault();
-        console.log(this.state);
-    }
+  _handleInputChange = event => {
+    const {
+      target: { value, name }
+    } = event;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  _handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  _handleFbLogin = response => {
+    console.log(response);
+  };
 }
 
 export default Container;
--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -1,57 +1,92 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import IconFb from 'react-ionicons/lib/LogoFacebook';
-import formStyles from '../../shared/formStyles.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import FacebookLogin from "react-facebook-login";
+import formStyles from "../../shared/formStyles.scss";
 
 const SignupForm = (props, context) => (
-    <div className={formStyles.signupWrap}>
-        <p className={formStyles.signupText}>
-            {context.t("Sign up to see photos and videos from your friends.")}
-        </p>
-        <button type="button" className={formStyles.buttonFb2}>
-            <IconFb fontSize="20px" color="#fff" className={formStyles.icon} />
-            {context.t("Log in with Facebook")}
-        </button>
-        <p className={formStyles.otherOption}>
-            <span className={formStyles.or}>{context.t("OR")}</span>
-        </p>
-        <form onSubmit={props.handleSubmit}>
-            <div className={formStyles.inputField}>
-                <input type="text" name="" id="signupId" placeholder={context.t("Mobile Number, or Email")} value={props.emailValue} name="email" onChange={props.handleInputChange} />
-            </div>
-            <div className={formStyles.inputField}>
-                <input type="text" name="" id="signupName" placeholder={context.t("Full Name")} value={props.fullnameValue} name="fullname" onChange={props.handleInputChange} />
-            </div>
-            <div className={formStyles.inputField}>
-                <input type="text" name="" id="signupNick" placeholder={context.t("Username")} value={props.usernameValue} name="username" onChange={props.handleInputChange} />
-            </div>
-            <div className={formStyles.inputField}>
-                <input type="password" name="" id="signupPw" placeholder={context.t("Password")} value={props.passwordValue} name="password" onChange={props.handleInputChange} />
-            </div>
-            <div className={formStyles.buttonWrap}>
-                <button className={formStyles.button}>{context.t("Sign Up")}</button>
-            </div>
-        </form>
-        <p className={formStyles.agreeText}>
-            By signing up, you agree to our
-            &nbsp;<strong>Terms</strong>,
-            &nbsp;<strong>Data Policy</strong> and
-            &nbsp;<strong>Cookies Policy</strong>.
-        </p>
-    </div>
+  <div className={formStyles.signupWrap}>
+    <p className={formStyles.signupText}>
+      {context.t("Sign up to see photos and videos from your friends.")}
+    </p>
+    <FacebookLogin
+      appId="2228520780713829"
+      autoLoad={true}
+      fields="name,email,picture"
+      callback={props.handleFbLogin}
+      cssClass={formStyles.buttonFb2}
+      icon="fa-facebook-square"
+    />
+    <p className={formStyles.otherOption}>
+      <span className={formStyles.or}>{context.t("OR")}</span>
+    </p>
+    <form onSubmit={props.handleSubmit}>
+      <div className={formStyles.inputField}>
+        <input
+          type="text"
+          name=""
+          id="signupId"
+          placeholder={context.t("Mobile Number, or Email")}
+          value={props.emailValue}
+          name="email"
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <div className={formStyles.inputField}>
+        <input
+          type="text"
+          name=""
+          id="signupName"
+          placeholder={context.t("Full Name")}
+          value={props.fullnameValue}
+          name="fullname"
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <div className={formStyles.inputField}>
+        <input
+          type="text"
+          name=""
+          id="signupNick"
+          placeholder={context.t("Username")}
+          value={props.usernameValue}
+          name="username"
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <div className={formStyles.inputField}>
+        <input
+          type="password"
+          name=""
+          id="signupPw"
+          placeholder={context.t("Password")}
+          value={props.passwordValue}
+          name="password"
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <div className={formStyles.buttonWrap}>
+        <button className={formStyles.button}>{context.t("Sign Up")}</button>
+      </div>
+    </form>
+    <p className={formStyles.agreeText}>
+      By signing up, you agree to our &nbsp;<strong>Terms</strong>, &nbsp;
+      <strong>Data Policy</strong> and &nbsp;<strong>Cookies Policy</strong>.
+    </p>
+  </div>
 );
 
 SignupForm.propTypes = {
-    emailValue: PropTypes.string.isRequired,
-    fullnameValue: PropTypes.string.isRequired,
-    usernameValue: PropTypes.string.isRequired,
-    passwordValue: PropTypes.string.isRequired,
-    handleInputChange: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
+  emailValue: PropTypes.string.isRequired,
+  fullnameValue: PropTypes.string.isRequired,
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleFbLogin: PropTypes.func.isRequired
 };
 
 SignupForm.contextTypes = {
-    t: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired
 };
 
 export default SignupForm;
