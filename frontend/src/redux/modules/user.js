3c5fa013407b9449277f099a59e78015c988df32
jin-2 <jmakeaz@gmail.com>
2019-01-20 19:54:36 +0900

    #3-38 Setting the Token with FB Login

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -1,8 +1,15 @@
 // import
 
 // action
+const SAVE_TOKEN = "SAVE_TOKEN";
 
 // action creator
+function saveToken(token) {
+  return {
+    type: SAVE_TOKEN,
+    token
+  };
+}
 
 // API actions
 function facebookLogin(access_token) {
@@ -17,25 +24,39 @@ function facebookLogin(access_token) {
       })
     })
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => {
+        console.log(json.token);
+        localStorage.setItem("jwt", json.token);
+        dispatch(saveToken(json.token));
+      })
       .catch(err => console.log(err));
   };
 }
 
 // initial state
 const initialState = {
-  isLoggedIn: localStorage.getItem("jwt") || false
+  isLoggedIn: localStorage.getItem("jwt") ? true : false
 };
 
 // reducer
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case "SAVE_TOKEN":
+      return applySetToken(state, action);
     default:
       return state;
   }
 }
 
 // reducer function
+function applySetToken(state, action) {
+  const { token } = action;
+  return {
+    ...state,
+    isLoggedIn: true,
+    token
+  };
+}
 
 // exports
 const actionCreators = {
